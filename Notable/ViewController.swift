f7106480f5d30e0d6baa0caaaa3cd6b186d10fc9
b3nhaddad <98062395+b3nhaddad@users.noreply.github.com>
2025-02-22 12:21:22 -0700

    Base Template for Notable

---
+++ Notable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notable
+//
+//  Created by Benjamin Haddad on 2/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
