7e6c903e1a72999271bc5e3c92ae4b5a8a4dbd88
b3nhaddad <98062395+b3nhaddad@users.noreply.github.com>
2025-02-22 14:16:36 -0700

    Update AppDelegate.swift

--- Notable/AppDelegate.swift
+++ Notable/AppDelegate.swift
@@ -1,40 +1,50 @@
-//
-//  AppDelegate.swift
-//  Notable
-//
-//  Created by Benjamin Haddad on 2/4/25.
-//
-
 import UIKit
-import FirebaseCore
-@main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+import GoogleMobileAds
 
+class ShowApp: UIViewController, FullScreenContentDelegate {
+    private var rewardedAd: RewardedAd?
+    private let adUnitID = "ca-app-pub-3940256099942544/1712485313" // Replace with your AdMob Rewarded Ad unit ID
 
-
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions:
-                     [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-                     FirebaseApp.configure()
-
-                     return true
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        loadRewardedAd()
+        
+        let showAdButton = UIButton(type: .system)
+        showAdButton.setTitle("Show Rewarded Ad", for: .normal)
+        showAdButton.addTarget(self, action: #selector(showRewardedAd), for: .touchUpInside)
+        showAdButton.frame = CGRect(x: 50, y: 200, width: 250, height: 50)
+        view.addSubview(showAdButton)
     }
 
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    func loadRewardedAd() {
+        RewardedAd.load(with: adUnitID, request: Request()) { ad, error in
+            if let error = error {
+                print("Failed to load rewarded ad: \(error.localizedDescription)")
+                return
+            }
+            self.rewardedAd = ad
+            self.rewardedAd?.fullScreenContentDelegate = self
+            print("Rewarded ad loaded successfully.")
+        }
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    @objc func showRewardedAd() {
+        if let ad = rewardedAd {
+            ad.present(from: self) {
+                let reward = ad.adReward
+                print("User rewarded with \(reward.amount) \(reward.type)")
+            }
+        } else {
+            print("Ad wasn't ready.")
+            loadRewardedAd() // Attempt to reload the ad
+        }
     }
 
-
+    func adDidDismissFullScreenContent(_ ad: FullScreenPresentingAd) {
+        print("Ad dismissed. Loading new ad.")
+        loadRewardedAd()
+    }
 }
 
 
